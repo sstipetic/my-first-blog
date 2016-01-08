5c7d8bd56d51ebf376d7cc61652100c521ab6312
sstipetic <stjepan.stipetic@fer.hr>
2016-01-08 18:21:23 +0100

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
+
 # import matlab.engine
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    me = User.objects.get(username = 'admin')
+    posts = Post.objects.filter(author=me).order_by('published_date')
     # eng = matlab.engine.start_matlab()
     # tf = eng.isprime(37)
     # return render(request, 'blog/post_list.html', {'posts': posts, 'tf': tf})
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
