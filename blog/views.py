9d2708e81fa80caba7f18259a2028f70de9f66dc
sstipetic <stjepan.stipetic@fer.hr>
2016-01-08 16:56:37 +0100

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+import matlab.engine
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # eng = matlab.engine.start_matlab()
+    # tf = eng.isprime(37)
+    # return render(request, 'blog/post_list.html', {'posts': posts, 'tf': tf})
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
